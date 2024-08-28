0f7a528ca7ee59488dc2589fc25b2b3396e2a211
paulomorenodev <23208589@aluno.univesp.br>
2024-08-28 15:13:43 -0300

    param_url_after_class_49

--- cars/views.py
+++ cars/views.py
@@ -4,9 +4,19 @@ from cars.models import Car
 
 def cars_view(request):
 
+    print(request)
+    print(type(request))
+    req = request.GET.get('search')
+    print(req)
+    print(request.GET)
+    ## Sintaxe para passar parametros
+    #http://localhost:8000/cars/?search=bily&name=john
+
     cars = Car.objects.all()
-    print(cars)
 
-
+    #cars = Car.objects.filter(model__contains='opa')
+    #cars = Car.objects.filter(brand__name='fiat')
+    #cars = Car.objects.filter(model='kwid')
+    
 
     return render(request,'cars.html',{'cars': cars})
