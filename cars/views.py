057030539f047a3dbb6caa36b9b77078d001ac67
paulomorenodev <23208589@aluno.univesp.br>
2024-08-28 16:03:36 -0300

    new_page_after_class_52

--- cars/views.py
+++ cars/views.py
@@ -3,20 +3,13 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all().order_by('model_year')#com o sinal de menos antes do model inverte a seleção
+    search = request.GET.get('search')
 
-    print(request)
-    print(type(request))
-    req = request.GET.get('search')
-    print(req)
-    print(request.GET)
-    ## Sintaxe para passar parametros
-    #http://localhost:8000/cars/?search=bily&name=john
+    if search:
+        cars = Car.objects.filter(model__contains=search)
+# outra opção abaixo para o codigo
+#Car.objects.filter(model__contains=search) if search else Car.objects.all() 
 
-    cars = Car.objects.all()
-
-    #cars = Car.objects.filter(model__contains='opa')
-    #cars = Car.objects.filter(brand__name='fiat')
-    #cars = Car.objects.filter(model='kwid')
-    
 
     return render(request,'cars.html',{'cars': cars})
