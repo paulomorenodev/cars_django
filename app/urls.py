86a3f2a079ad6dc16f088e6f9696b16f0334e1ad
paulomorenodev <23208589@aluno.univesp.br>
2024-09-01 11:01:23 -0300

    listview_class_67

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewcarView
+from cars.views import NewcarView, CarsListView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -11,6 +11,6 @@ urlpatterns = [
     path('register/', register_view, name= 'register'),
     path('login/', login_view, name= 'login'),
     path('logout/', logout_view, name= 'logout'),
-    path('cars/', CarsView.as_view(), name= 'cars_list'),
+    path('cars/', CarsListView.as_view(), name= 'cars_list'),
     path('new_car/', NewcarView.as_view(), name= 'new_car')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,19 +1,23 @@
 
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import ListView
 from cars.models import Car
 from cars.forms import CardModelForm
 
 
-class CarsView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by('model_year')
-        search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
         if search:
             cars = cars.filter(model__icontains=search)
+        return cars
 
-        return render(request,'cars.html',{'cars': cars})
 
 class NewcarView(View):
 
