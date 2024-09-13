78e6ee9d4aa7151531024ec6d41ce9dba2c9220d
paulomorenodev <23208589@aluno.univesp.br>
2024-09-13 04:23:45 -0300

    update_inventory_class_78

--- cars/signals.py
+++ cars/signals.py
@@ -1,24 +1,24 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete 
+from django.db.models.signals import post_save, post_delete 
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('*** pre save ***')
-    print(instance)
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count = cars_count,
+        cars_value = cars_value
+    )
+
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('*** post save ***')
-    print(instance)
-
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('*** pre delete ***')
-    print(instance)
+    car_inventory_update()
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print('*** post delete ***')
-    print(instance)
+    car_inventory_update()
 
